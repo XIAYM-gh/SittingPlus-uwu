57514c2f39e91084e1c0e4c1a34bf2baafb9f236
XIAYM-gh <xiaym123@qq.com>
2025-07-12 17:34:39 +0800

    Update language & Improve server-side logic

--- src/main/java/uwu/sittingplus/SittingPlusInitializer.java
+++ src/main/java/uwu/sittingplus/SittingPlusInitializer.java
@@ -10,6 +10,8 @@ import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uwu.sittingplus.payload.c2s.HelloV1C2SPayload;
 import uwu.sittingplus.payload.c2s.SitV1C2SPayload;
 import uwu.sittingplus.payload.c2s.StopSitV1C2SPayload;
@@ -17,16 +19,16 @@ import uwu.sittingplus.payload.s2c.HelloV1S2CPayload;
 import uwu.sittingplus.payload.s2c.PoseSyncV1S2CPayload;
 import uwu.sittingplus.payload.s2c.StopSitV1S2CPayload;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class SittingPlusInitializer implements ModInitializer {
     private static final HashMap<UUID, String> PLAYER_SIT_POSES = new HashMap<>();
+    private static final ArrayList<UUID> PENDING = new ArrayList<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(SittingPlusInitializer.class);
 
     private static void reset() {
         PLAYER_SIT_POSES.clear();
+        PENDING.clear();
     }
 
     @SuppressWarnings("resource")
@@ -44,8 +46,23 @@ public class SittingPlusInitializer implements ModInitializer {
         playS2C.register(StopSitV1S2CPayload.ID, StopSitV1S2CPayload.CODEC);
 
         // Payload Handling
-        ServerPlayNetworking.registerGlobalReceiver(HelloV1C2SPayload.ID, (payload, context) -> context.responseSender()
-                .sendPacket(new HelloV1S2CPayload(Constants.PROTOCOL_VERSION)));
+        ServerPlayNetworking.registerGlobalReceiver(HelloV1C2SPayload.ID, (payload, context) -> {
+            context.responseSender().sendPacket(new HelloV1S2CPayload(Constants.PROTOCOL_VERSION));
+
+            if (payload.protocolVersion() != Constants.PROTOCOL_VERSION) {
+                LOGGER.warn("Player {} joined with a mismatch SittingPlus-uwu client!", context.player().getName());
+                return;
+            }
+
+            UUID uuid = context.player().getUuid();
+            if (PENDING.contains(uuid)) {
+                PENDING.remove(uuid);
+                for (Map.Entry<UUID, String> entry : PLAYER_SIT_POSES.entrySet()) {
+                    context.responseSender().sendPacket(new PoseSyncV1S2CPayload(entry.getKey(), entry.getValue()));
+                }
+            }
+        });
+
         ServerPlayNetworking.registerGlobalReceiver(SitV1C2SPayload.ID, (payload, context) -> {
             UUID uuid = context.player().getUuid();
             String pose = payload.identifierPath();
@@ -55,6 +72,7 @@ public class SittingPlusInitializer implements ModInitializer {
             context.server().getPlayerManager()
                     .sendToAll(ServerPlayNetworking.createS2CPacket(new PoseSyncV1S2CPayload(uuid, pose)));
         });
+
         ServerPlayNetworking.registerGlobalReceiver(StopSitV1C2SPayload.ID, (payload, context) -> handleStopSit(context.server(), context.player()
                 .getUuid()));
 
@@ -62,9 +80,10 @@ public class SittingPlusInitializer implements ModInitializer {
         ServerLifecycleEvents.SERVER_STOPPING.register(server -> reset());
 
         // Sync to new players
-        ServerPlayConnectionEvents.JOIN.register((_handler, sender, _server) -> {
-            for (Map.Entry<UUID, String> entry : PLAYER_SIT_POSES.entrySet()) {
-                sender.sendPacket(new PoseSyncV1S2CPayload(entry.getKey(), entry.getValue()));
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, _server) -> {
+            UUID uuid = handler.player.getUuid();
+            if (!PENDING.contains(uuid)) {
+                PENDING.add(uuid);
             }
         });
 
